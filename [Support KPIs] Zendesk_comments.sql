06830edbd053bc71bd84375f3a448e7069aba29f
Hugo Dornbierer <hugo@drivy.com>
2017-03-03 18:44:58 +0100

    Add files via upload

---
+++ [Support KPIs] Zendesk_comments.sql
@@ -0,0 +1,32 @@
+select *
+
+	from (select ztc.id, ztc.ticket_id, ztc.is_public, ztc.channel, u.first_name as agent, to_char(ztc.created_at, 'DD') as day, to_char(ztc.created_at, 'IW') as week, to_char(ztc.created_at, 'MM') as month, to_char(ztc.created_at, 'YYYY') as year
+
+			from zendesk_ticket_comments ztc
+			inner join zendesk_users zu on zu.id = ztc.author_id
+			inner join users u on zu.drivy_user_id = u.id
+			inner join (select *
+
+							from
+
+							(select z.id
+
+								from zendesk_tickets z
+								left join (select * from zendesk_ticket_tags zz where zz.tag_name like '%rental_flag%' or zz.tag_name like '%match_flag%' or zz.tag_name like '%car_flag%' or zz.tag_name like '%rental_message_flag%' or zz.tag_name like 											'%rental_message_flags%' or zz.tag_name like '%car_deactivation%' or zz.tag_name like '%sms_failure%' or zz.tag_name like '%car_flags%' or zz.tag_name like '%open_checkin_checkout%' group by zz.tag_name, zz.ticket_id)zt on 										zt.ticket_id = z.id
+								left join zendesk_users zu on zu.id = z.submitter_id
+
+									where z.group_id in ('25094752','20731033','22929952','24306629','24661835','23603861','24478605','24788545','24661835','23603861','24478605','24788545','24527022','24408869','24902729','26300089','24808509', '26344589', 										'26601549')
+									and (zu.role = 'end-user' or zu.role='agent')
+									and zt.ticket_id is null
+	
+										order by z.created_at asc)t
+
+							)t on t.id = ztc.ticket_id
+
+				where ztc.is_public is true
+				and (zu.role='agent' or zu.role = 'admin')
+				and u.locale = 'fr')i
+		
+		where year = 2017
+		and week = ---
+		order by year asc, week asc, day asc
