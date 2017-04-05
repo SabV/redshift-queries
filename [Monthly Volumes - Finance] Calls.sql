c5c47e1318217be0686f16450f1d97a8759e8a59
Hugo Dornbierer <hugo@drivy.com>
2017-04-05 12:40:39 +0200

    Update [Monthly Volumes - Finance] Calls.sql

--- [Monthly Volumes - Finance] Calls.sql
+++ [Monthly Volumes - Finance] Calls.sql
@@ -3,7 +3,7 @@ select t.id, t.phone_display_name as group, t.agent_name as agent, to_char(conve
 	from talkdesk_calls t
 	
 		where to_char(convert_timezone('Europe/Paris', t.start_time), 'YYYY') = 2017
-		and to_char(convert_timezone('Europe/Paris', t.start_time), 'MM') = '03'
+		and to_char(convert_timezone('Europe/Paris', t.start_time), 'MM') = ---
 
 			order by t.start_time asc
 ;
