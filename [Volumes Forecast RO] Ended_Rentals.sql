3dfe0fd99903cb3abe8568b0a03a8683f19aa183
Hugo Dornbierer <hugo@drivy.com>
2017-04-19 15:47:50 +0200

    Update and rename Untitled Query.sql to [Volumes Forecast RO] Ended_Rentals.sql

---
+++ [Volumes Forecast RO] Ended_Rentals.sql
@@ -0,0 +1,9 @@
+select count(1), to_char(convert_timezone('Europe/Paris', ends_at), 'IW')
+
+from rentals
+
+where to_char(convert_timezone('Europe/Paris', ends_at), 'IW') = ---
+and to_char(convert_timezone('Europe/Paris', ends_at), 'YYYY') = 2017
+and state = 'ended'
+
+group by to_char(convert_timezone('Europe/Paris', ends_at), 'IW')
