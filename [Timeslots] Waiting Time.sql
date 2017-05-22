431be02cb16c9dca684fbb60c20aaf7eddd88a84
Hugo Dornbierer <hugo@drivy.com>
2017-05-22 18:46:15 +0200

    Update [Timeslots] Waiting Time.sql

--- [Timeslots] Waiting Time.sql
+++ [Timeslots] Waiting Time.sql
@@ -30,8 +30,8 @@ AND date_trunc('week', convert_timezone('Europe/Paris', t.start_time)) >= date_t
                                                  'DE Open Support',
                                                  'AT Support')
                         AND NOT (t.phone_display_name = 'DE Support'
-                                 AND t.ivr_options = '3')) THEN 'DE-AT' WHEN t.phone_display_name IN ('ES Support',
-                                                                                                      'ES Open Support') THEN 'ES' WHEN t.phone_display_name = 'BE Support' THEN 'BE' ELSE 'ELSE' END AS country,
+                                 AND t.ivr_options in (1, 3))) THEN 'DE-AT' WHEN (t.phone_display_name IN ('ES Support',
+                                                                                                      'ES Open Support') and t.ivr_options <> 1) THEN 'ES' WHEN (t.phone_display_name = 'BE Support' and t.ivr_options <> 1) THEN 'BE' ELSE 'ELSE' END AS country,
 date_trunc('week', convert_timezone('Europe/Paris', t.start_time)) AS year_week,
           to_char(convert_timezone('Europe/Paris', t.start_time), 'D') as day,
           to_char(convert_timezone('Europe/Paris', t.start_time), 'HH24') as hour -- Countries (Waiting Time)
