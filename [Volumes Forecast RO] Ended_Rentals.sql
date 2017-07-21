29490095c38cbe881606cee0b5f2c73cfc25c38d
Hugo Dornbierer <hugo@drivy.com>
2017-07-21 17:02:56 +0200

    Delete [Volumes Forecast RO] Ended_Rentals.sql

--- [Volumes Forecast RO] Ended_Rentals.sql
+++
@@ -1,9 +0,0 @@
-select count(1)
-
-from rentals
-
-where to_char(convert_timezone('Europe/Paris', ends_at), 'IW') = ---
-and to_char(convert_timezone('Europe/Paris', ends_at), 'YYYY') = 2017
-and state = 'ended'
-
-group by to_char(convert_timezone('Europe/Paris', ends_at), 'IW')
