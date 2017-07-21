d6d007552158ef0d431ad2c0ff1e9a1c418eef10
Hugo Dornbierer <hugo@drivy.com>
2017-07-21 16:32:51 +0200

    Add files via upload

---
+++ Finance Tickets.sql
@@ -0,0 +1,19 @@
+select *
+ 
+	from
+
+	(select z.id, zg.name, a.first_name as agent, to_char(convert_timezone('Europe/Paris', z.created_at), 'MM') as month, 	to_char(convert_timezone('Europe/Paris', z.created_at), 'YYYY') as year
+
+		from zendesk_tickets z
+		left join zendesk_users zu on zu.id=z.submitter_id
+		left join zendesk_users za on za.id=z.assignee_id
+		left join zendesk_groups zg on zg.id = z.group_id
+		left join users a on a.id=za.drivy_user_id
+
+			where (zu.role = 'end-user' or zu.role='agent')
+
+				order by z.created_at asc)t
+
+	where t.year = 2017
+	and t.month = ---
+;
